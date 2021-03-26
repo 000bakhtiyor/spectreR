56ba33330c4e8e017d1cbbb0d3de867058d6d7f6
jamshid <jamshidjabbarov17@gmail.com>
2021-03-26 15:03:34 +0500

    forms

--- AdsR/users/forms.py
+++ AdsR/users/forms.py
@@ -1,6 +1,8 @@
-from django import forms 
+from django import forms
+from django.forms import ModelForm 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -10,3 +12,9 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email','password1','password2']
+
+
+class ProfileCreationForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields  = ['phonenumber_code', 'phonenumber']
--- AdsR/users/models.py
+++ AdsR/users/models.py
@@ -1,7 +1,30 @@
 from django.db import models
-from django.contrib.urls.models import User
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 # Create your models here.
 
+def validate_number(number):
+    if len(str(number)) == 7:
+        return number
+    else:
+        raise ValidationError("Telefon raqam 7 raqamdan iborat bulishi kerak!")   
+
 class Profile(models.Model):
+    code_choices = (
+        ('88',88),
+        ('90',90),
+        ('91',91), 
+        ('93',93),
+        ('94',94),
+        ('95',95),
+        ('98',98),
+        ('99',99)
+    )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    card = n
+    phonenumber_code = models.CharField(max_length=2, choices=code_choices, default=91)
+    phonenumber = models.IntegerField(validators=[validate_number])
+
+    def __str__(self):
+        return "{}-profile".format(self.user.username)
+    
+ 
